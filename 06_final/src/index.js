56c766ee7c558ee3cfa3ca19baa61a7da2246886
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-03-25 11:08:34 +0100

    integrated next and previous callbacks

--- 06_final/src/index.js
+++ 06_final/src/index.js
@@ -1,1 +1,22 @@
-console.log('Yeah')
+const timeLineItems = document.querySelectorAll('.timeline__list__item')
+let index = 0
+
+const goTo = newIndex => {
+  index = newIndex
+}
+
+const next = () => {
+  if (index < timeLineItems.length - 1) {
+    goTo(index + 1)
+  }
+}
+
+const previous = () => {
+  if (index > 0) {
+    goTo(index - 1)
+  }
+}
+
+document.querySelector('[data-next]').addEventListener('click', next, false)
+document.querySelector('[data-previous]').addEventListener('click', previous, false)
+
